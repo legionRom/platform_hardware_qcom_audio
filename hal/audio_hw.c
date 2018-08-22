d4541f0cbb7c1c8aaf8e505639936c97cd272d31
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-21 21:59:07 -0700

    audio: Guard power@1.2 hints
    Not every device will implement these, so guard them behind
    AUDIO_FEATURE_ENABLED_PERF_HINTS
    
    Change-Id: If2f4e373f1ab2924d84b4a18ddce9f68e301e7c4
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -73,7 +73,9 @@
 #include "platform_api.h"
 #include <platform.h>
 #include "audio_extn.h"
+#ifdef PERF_HINTS_ENABLED
 #include "audio_perf.h"
+#endif
 #include "voice_extn.h"
 #include "ip_hdlr_intf.h"
 #include "audio_amplifier.h"
@@ -2525,7 +2527,9 @@ int start_input_stream(struct stream_in *in)
     uc_info->out_snd_device = SND_DEVICE_NONE;
 
     list_add_tail(&adev->usecase_list, &uc_info->list);
+#ifdef PERF_HINTS_ENABLED
     audio_streaming_hint_start();
+#endif
     audio_extn_perf_lock_acquire(&adev->perf_lock_handle, 0,
                                  adev->perf_lock_opts,
                                  adev->perf_lock_opts_size);
@@ -2620,14 +2624,18 @@ int start_input_stream(struct stream_in *in)
     check_and_enable_effect(adev);
 
 done_open:
+#ifdef PERF_HINTS_ENABLED
     audio_streaming_hint_end();
+#endif
     audio_extn_perf_lock_release(&adev->perf_lock_handle);
     ALOGD("%s: exit", __func__);
 
     return ret;
 
 error_open:
+#ifdef PERF_HINTS_ENABLED
     audio_streaming_hint_end();
+#endif
     audio_extn_perf_lock_release(&adev->perf_lock_handle);
     stop_input_stream(in);
 error_config:
@@ -3121,7 +3129,9 @@ int start_output_stream(struct stream_out *out)
 
     list_add_tail(&adev->usecase_list, &uc_info->list);
 
+#ifdef PERF_HINTS_ENABLED
     audio_streaming_hint_start();
+#endif
     audio_extn_perf_lock_acquire(&adev->perf_lock_handle, 0,
                                  adev->perf_lock_opts,
                                  adev->perf_lock_opts_size);
@@ -3313,7 +3323,9 @@ int start_output_stream(struct stream_out *out)
         }
     }
 
+#ifdef PERF_HINTS_ENABLED
     audio_streaming_hint_end();
+#endif
     audio_extn_perf_lock_release(&adev->perf_lock_handle);
     ALOGD("%s: exit", __func__);
 
@@ -3333,7 +3345,9 @@ int start_output_stream(struct stream_out *out)
     ATRACE_END();
     return ret;
 error_open:
+#ifdef PERF_HINTS_ENABLED
     audio_streaming_hint_end();
+#endif
     audio_extn_perf_lock_release(&adev->perf_lock_handle);
     stop_output_stream(out);
 error_config:
